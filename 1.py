45a3adc7aac1d21d8c78e98f9dce0a9d35ffdddb
SainadhPuriparthi <43814594+SainadhPuriparthi@users.noreply.github.com>
2018-10-03 18:34:16 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,11 @@
+"""
+"""
+s=["open source","libraries","portal","object oriented","embededable","easy to larn"]
+
+#str1 = 'Bangalore'
+      
+y = []
+for str1 in s:
+    print str1[0]
+    y.append(str1[0])
+print(y)
